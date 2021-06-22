9488d864107acf7d7df8e442a5b489786c60ff70
Techcable <Techcable@techcable.net>
2021-06-21 21:39:38 -0700

    Implement a command line interface to dump an AST

---
+++ src/bin/dump_ast.rs
@@ -0,0 +1,188 @@
+use clap::{Clap, ArgGroup, ValueHint};
+use pathos_python_parser::ParseMode;
+use std::path::{PathBuf};
+use anyhow::{Error, Context};
+use std::ffi::OsString;
+use tempfile::NamedTempFile;
+use pathos_python_parser::ast::Allocator;
+use bumpalo::Bump;
+use pathos_python_parser::ast::constants::ConstantPool;
+use pathos_python_parser::ast::ident::SymbolTable;
+use anyhow::{bail};
+use std::io::Read;
+
+fn main() -> anyhow::Result<()> {
+    let options: Options = Options::parse();
+    let input = options.input()?;
+    let text = input.read_input()?;
+    let arena = Allocator::new(Bump::new());
+    let mut pool = ConstantPool::new(&arena);
+    let mut symbols = SymbolTable::new(&arena);
+    let ast = pathos_python_parser::parse_text(
+        &arena,
+        &text,
+        options.parse_mode.map_or_else(|| input.default_parse_mode(), ParseMode::from),
+        &mut pool,
+        &mut symbols
+    ).with_context(|| format!("Failed to parse input"))?;
+    eprintln!("Pretty printed ast:");
+    match options.output_format.unwrap_or_else(Default::default) {
+        OutputFormat::Json => {
+            let out = std::io::stdout();
+            let out = out.lock();
+            ::serde_json::to_writer_pretty(
+                out,
+                &ast
+            )?;
+        },
+        OutputFormat::Debug => {
+            println!("{:#?}", ast)
+        }
+    }
+    Ok(())
+}
+
+#[derive(Clap)]
+pub enum OutputFormat {
+    Json,
+    Debug
+}
+impl Default for OutputFormat {
+    fn default() -> OutputFormat {
+        OutputFormat::Json
+    }
+}
+
+/// A simple command to parse Python sources,
+/// then dump its AST
+///
+/// By default, this dumps a JSON representation
+/// that should match the CPython AST.
+/// However, other representations should (eventually)
+/// be implemented.
+///
+/// See the (unofficial) documentation of the official AST:
+/// https://greentreesnakes.readthedocs.io/en/latest/nodes.html
+#[derive(Clap)]
+#[clap(
+    author, // Derive author from Cargo.toml
+    version, // Derive version from Cargo.toml
+    name = "dump-ast"
+)]
+#[clap(group = ArgGroup::new("input").required(true))]
+struct Options {
+    /// Give verbose output
+    #[clap(long, short = 'v')]
+    verbose: bool,
+    /// The format to output the parsed AST in
+    ///
+    /// By default, this is inferred
+    #[clap(long, arg_enum)]
+    output_format: Option<OutputFormat>,
+    /// The mode to parse the input in
+    #[clap(long, arg_enum)]
+    parse_mode: Option<ParseModeOpt>,
+    /// The input source file
+    #[clap(
+        long, short = 'c',
+        aliases = &["file"],
+        group = "input",
+        parse(from_os_str),
+        value_hint = ValueHint::FilePath
+    )]
+    input_file: Option<PathBuf>,
+    /// Explicitly given input text
+    #[clap(long = "text", group = "input")]
+    input_text: Option<String>,
+    /// Read the input from stdin
+    #[clap(long = "stdin", group = "input")]
+    read_stdin: bool,
+    /// Open the input in an editor
+    #[clap(long = "edit", group = "input")]
+    open_editor: bool,
+}
+
+impl Options {
+    fn input(&self) -> Result<Input, Error> {
+        if let Some(ref f) = self.input_file {
+            Ok(Input::File(f.clone()))
+        } else if let Some(ref t) = self.input_text {
+            Ok(Input::Text(t.clone()))
+        } else if self.read_stdin {
+            Ok(Input::Stdin)
+        } else if self.open_editor {
+            Ok(Input::Editor)
+        } else {
+            bail!("No input given")
+        }
+    }
+}
+enum Input {
+    File(PathBuf),
+    Text(String),
+    Stdin,
+    Editor
+}
+impl Input {
+    fn default_parse_mode(&self) -> ParseMode {
+        match *self {
+            Input::File(_) => ParseMode::Module,
+            Input::Text(_) => ParseMode::Expression,
+            Input::Stdin => ParseMode::Module,
+            Input::Editor => ParseMode::Module
+        }
+    }
+    fn read_input(&self) -> Result<String, Error> {
+        match *self {
+            Input::File(ref path) => {
+                let mut res = String::new();
+                std::fs::File::open(path).and_then(|mut f| f.read_to_string(&mut res))
+                    .with_context(|| format!("Failed to read file: {}", path.display()))?;
+                Ok(res)
+            }
+            Input::Text(ref t) => Ok(t.clone()),
+            Input::Stdin => {
+                let mut res = String::new();
+                let stdin = std::io::stdin();
+                let mut stdin = stdin.lock();
+                stdin.read_to_string(&mut res)
+                    .with_context(|| format!("Unable to read stdin"))?;
+                drop(stdin);
+                Ok(res)
+            },
+            Input::Editor => {
+                let editor = std::env::var_os("EDITOR")
+                    .unwrap_or_else(|| OsString::from("vi"));
+                let mut temp = NamedTempFile::new()?;
+                use std::process::{Command};
+                let mut proc = Command::new(&editor)
+                    .arg(temp.path().as_os_str())
+                    .spawn()
+                    .with_context(|| format!("Unable to spawn editor '{}'", editor.to_string_lossy()))?;
+                proc.wait().map_err(anyhow::Error::from)
+                    .and_then(|status| if !status.success() {
+                        bail!("Failure w/ status {}", status)
+                    } else { Ok(()) })
+                    .with_context(|| format!("Failed to run editor '{}'", editor.to_string_lossy()))?;
+                let mut res = String::new();
+                temp.read_to_string(&mut res)?;
+                Ok(res)
+            }
+        }
+    }
+}
+#[derive(Clap)]
+enum ParseModeOpt {
+    #[clap(alias = "expr", alias = "eval")]
+    Expression,
+    #[clap(alias = "mod", alias = "exec")]
+    Module,
+}
+impl From<ParseModeOpt> for ParseMode {
+    fn from(opt: ParseModeOpt) -> Self {
+       match opt {
+           ParseModeOpt::Expression => ParseMode::Expression,
+           ParseModeOpt::Module => ParseMode::Module,
+       }
+    }
+}
